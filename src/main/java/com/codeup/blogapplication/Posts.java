ab9d324e2327a94d722a160e4da99b8d442578e6
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-11-30 09:39:20 -0600

    created getters and setters on post class

--- src/main/java/com/codeup/blogapplication/Posts.java
+++ src/main/java/com/codeup/blogapplication/Posts.java
@@ -1,4 +1,24 @@
 package com.codeup.blogapplication;
 
 public class Posts {
+    private String title;
+    private String body;
+
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setBody(String body){
+        this.body = body;
+    }
 }
